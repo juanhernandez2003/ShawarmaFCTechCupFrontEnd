43a22a02b6b2ea4a1956a170992dcb5c35cd265c
Daniel Felipe Rayo Rodriguez <daniel.rayo@mail.escuelaing.edu.co>
2026-04-04 18:52:27 -0500

    :cat: subo el scalefolding inicial con las rutas de inicio de sesion y rutas protegidas

---
+++ src/features/teams/TeamDetailPage.jsx
@@ -0,0 +1,5 @@
+const TeamDetailPage = () => {//placeholder
+    return <div>Detalle del Equipo</div>
+}
+
+export default TeamDetailPage
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {//placeholder
+    return <div>404 - PÃ¡gina no encontrada</div>
+}
+
+export default NotFoundPage
